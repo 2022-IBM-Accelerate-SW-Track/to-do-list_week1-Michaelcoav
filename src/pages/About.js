3a1edd5fea89fcdc80a2a4666078f5334c624ffe
Michaelcoav <michaelcoav@gmail.com>
2022-06-08 20:47:33 -0400

    Edit About Me page, add node files, and assets

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import './About.css';
+import imgSrc from "../assets/MichaelCoca.png";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <img className="profile_image left centered" src={imgSrc} alt="Michael" />
+        </div>
+        
+        <div className="split right">
+          <div className="centered">
+            <h1 className="name_title">Michael Coca-Vargas</h1>
+            <p className="brief_description">My name is Michael Coca-Vargas, I am a rising junior at George Mason University. My hobbies include playing video games, watching anime, and coding.</p>
+          </div>
+        </div>
+        
       </div>
     )
   }
